f0822dc33ba4b86f15aa82693ffae728755b35c0
Yourproductpal <132340567+Yourproductpal@users.noreply.github.com>
2023-06-23 11:01:26 +0100

    fixing conflicts

--- wordpress_xmlrpc/base.py
+++ wordpress_xmlrpc/base.py
@@ -44,19 +44,6 @@ class Client(object):
                 raise XmlrpcDisabledError(e.faultString)
             else:
                 raise
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-        return method.process_result(raw_result.decode('utf-8'))
-=======
-        return method.process_result(raw_result)
->>>>>>> parent of 6358287 (changes)
-=======
-        return method.process_result(raw_result.encode('utf-8'))
->>>>>>> parent of 495ff32 (fixing)
-=======
-        return method.process_result(raw_result)
->>>>>>> parent of 6358287 (changes)
 
 
 class XmlrpcMethod(object):
